091e2829ad0d34ee283ee5c004cc120cdf851f13
Mohammad Ali Khan <mohammad_ali93@live.com>
2013-10-16 20:06:31 +0100

    Better, non-breaking-everything commit.

--- helloworld.py
+++ helloworld.py
@@ -4,7 +4,7 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write("Hello, World!\n")
-        self.response.write('Ali was also here, hah')
+        self.response.write('\nAli was also here, hah (sorry about the \'breaking the whole thing\' bit tho)')
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
