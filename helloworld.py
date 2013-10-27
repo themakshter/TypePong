81b05a337b80b3be2b09cd093dcb48f04d416ffa
Henco Appel <hencoappel@gmail.com>
2013-10-27 15:24:06 +0000

    First commit of the HTML UI

--- helloworld.py
+++ helloworld.py
@@ -6,6 +6,13 @@ class MainPage(webapp2.RequestHandler):
         self.response.write("Hello, World!\n")
         self.response.write('\nAli was also here, hah (sorry about the \'breaking the whole thing\' bit tho)')
 
+class Test(RequestHandler):
+	def get(self):
+		template = Template(filename='test.html')
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(template.render())
+
 application = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/test', Test),
 ], debug=True)
