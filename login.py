cebc872b2933ec79c273faf1f738c242e10a8209
aelred <aelred717@gmail.com>
2013-11-19 18:46:51 +0000

    ADD: Adding support for "detail" field for login

--- login.py
+++ login.py
@@ -12,46 +12,46 @@ class LoginException(Exception):
     def __str__(self):
         return self.msg
 
-class UsernameDoesNotExist(LoginException):
-    def __init__(self, username):
-        self.msg = 'Username %s does not exist' % username
+class UserDoesNotExist(LoginException):
+    def __init__(self, detail):
+        self.msg = 'User %s does not exist with login detail' % detail
 
-class InvalidUsername(LoginException):
-    def __init__(self, username, msg):
-        self.msg = 'Invalid username %s: %s' % username, msg
+class InvalidLoginDetail(LoginException):
+    def __init__(self, detail, msg):
+        self.msg = 'Invalid login detail %s: %s' % detail, msg
 
 class InvalidPassword(LoginException):
     def __init__(self, msg):
         self.msg = 'Invalid password: %s' % msg
 
-def _read_user(username):
-    '''returns data on a specific username. Raises a UsernameDoesNotExist exception if username doesn't exist'''
-    for this_user, this_pass, this_salt in csv.reader(open(_file, 'r')):
-        if username == this_user:
-            return this_user, this_pass, this_salt
+def _read_user(detail):
+    '''returns data on a specific login detail. Raises a UserDoesNotExist exception if login detail doesn't exist'''
+    for this_name, this_detail, this_pass, this_salt in csv.reader(open(_file, 'r')):
+        if detail == this_detail:
+            return this_name, this_detail, this_pass, this_salt
 
-    raise UsernameDoesNotExist(username)
+    raise UserDoesNotExist(detail)
 
-def _write_user(username, pass_hash, salt):
+def _write_user(name, detail, pass_hash, salt):
     '''writes new user to users database'''
     writer = csv.writer(open(_file, 'a+'))
-    writer.writerow((username, pass_hash, salt))
+    writer.writerow((name, detail, pass_hash, salt))
 
-def validate_username(username):
-    '''tests if username is valid'''
-    if username_exists(username):
-        raise InvalidUsername(username, 'username already exists')
+def validate_detail(detail):
+    '''tests if login detail is valid'''
+    if detail_exists(detail):
+        raise InvalidLoginDetail(detail, 'login detail already exists')
 
 def validate_password(password):
     '''tests if this is a valid password. Raises exceptions if invalid'''
     if len(password) < _min_length:
         raise InvalidPassword('must be at least %s characters' % _min_length)
 
-def username_exists(username):
-    '''tests if this username is already in use'''
+def detail_exists(detail):
+    '''tests if this login detail is already in use'''
     try:
-        _read_user(username)
-    except UsernameDoesNotExist:
+        _read_user(detail)
+    except UserDoesNotExist:
         return False
     else:
         return True
@@ -61,20 +61,20 @@ def hash_password(password, salt):
     salted_pass = password + salt
     return hashlib.sha256(salted_pass).hexdigest()
 
-def register(username, password):
-    '''registers a user. May raise InvalidUsername or InvalidPassword'''
-    validate_username(username)
+def register(name, detail, password):
+    '''registers a user. May raise InvalidLoginDetail or InvalidPassword'''
+    validate_detail(detail)
     validate_password(password)
 
     salt = "".join([random.choice(string.hexdigits) for i in xrange(_salt_length)])
     pass_hash = hash_password(password, salt)
 
-    _write_user(username, pass_hash, salt)
+    _write_user(name, detail, pass_hash, salt)
 
-def login(username, password):
-    '''logs the user in, returns false if password is incorrect and raises a UsernameDoesNotExist exception if username doesn't exist'''
+def login(detail, password):
+    '''logs the user in, returns false if password is incorrect and raises a UserDoesNotExist exception if login detail doesn't exist'''
 
-    this_user, this_pass, this_salt = _read_user(username)
+    (this_name, this_detail, this_pass, this_salt) = _read_user(detail)
 
     pass_hash = hash_password(password, this_salt)
 
