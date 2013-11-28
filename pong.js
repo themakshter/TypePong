7dab3c4fa0c14fa6f00ef2e0eb75922beb6d72cf
Mohammad Ali Khan <mohammad_ali93@live.com>
2013-11-28 22:33:11 +0000

    AI and Tony/Sebi changes

--- pong.js
+++ pong.js
@@ -8,28 +8,104 @@ var canvas_width = 750;
 var canvas_height = 500;
 var x_startPos = 350;
 var y_startPos = 350;
-var cpu = new Paddle(50, 200);
-var player = new Paddle(700, 200);
 var circle_radius = 10;
+var wordList = [];
+var currentWords = [];
 
-function Paddle(xPos, yPos) {
+var updateWords = function (id) {
+    'use strict';
+
+    if (wordList.length === 0) {
+        fetchWords();
+    }
+    currentWords[id] = wordList.pop();
+    var p_id = '#current_' + id;
+    $(p_id).html((id + 1) + ": " + currentWords[id]);
+};
+
+var colorify = function (s, color) {
+    'use strict';
+
+    s = s.replace(/<span[^>]+>([^<>\s]+)<\/span>/, "$1");
+    return '<span style="color: ' + color +
+        '">' + s + '</span>';
+};
+
+var updateColor = function (id, color) {
+    'use strict';
+
+    var p_id = '#current_' + id;
+    $(p_id).html((id + 1) + ": " + colorify(currentWords[id], color));
+};
+
+var resetAllColors = function () {
+    'use strict';
+
+    var id;
+    for (id = 0; id < currentWords.length; id += 1) {
+        updateColor(id, 'black');
+    }
+};
+
+var markPositions = function (n) {
+    'use strict';
+
+    var i, pos = [];
+    for (i = 0; i < n; i += 1) {
+        pos.push(canvas_height * (i + 1) / n - canvas_height / (2 * n));
+    }
+    return pos;
+};
+
+var drawPositions = function (ctx, pos) {
+    'use strict';
+
+    var i;
+    for (i = 0; i < pos.length; i += 1) {
+        ctx.font = "18px Terminator Two";
+        ctx.fillStyle = "white";
+        ctx.fillText((i + 1).toString(), 650, pos[i]);
+    }
+};
+
+var Paddle = function (xPos, yPos) {
+    'use strict';
+
     this.xPos = xPos;
     this.yPos = yPos;
     this.reqyPos = yPos;
-    this.dy = 3;
+    this.dy = 2;
     this.width = 20;
-    this.height = 100;
+    this.height = 85;
     this.score = 0;
+
     this.hitsHorizontalFace = function (x, y) {
-        return (((x >= (this.xPos + this.width) && x <= (this.xPos + this.width - dx + circle_radius)) || (x <= this.xPos && x >= this.xPos - dx - circle_radius)) && ((y >= this.yPos - circle_radius && y <= (this.yPos + this.height + circle_radius))));
+        var startX, endX, startY, endY;
+        startX = this.xPos + this.width;
+        endX = startX - dx + circle_radius;
+        startY = this.yPos - circle_radius;
+        endY = this.yPos + this.height + circle_radius;
+        return (((x >= startX && x <= endX) || (x <= this.xPos && x >= this.xPos - dx - circle_radius)) && (y >= startY && y <= endY));
     };
     this.hitsVerticalFace = function (x, y) {
-        return (((y >= (this.yPos + this.height) && y <= (this.yPos + this.height - dy + circle_radius)) || (y <= this.yPos && y >= this.yPos - dy - circle_radius)) && ((x >= this.xPos - circle_radius && x <= (this.xPos + this.width + circle_radius))));
+        var startX, endX, startY, endY;
+        startY = this.yPos + this.height;
+        endY = startY - dy + circle_radius;
+        startX = this.xPos - circle_radius;
+        endX = this.xPos + this.width + circle_radius;
+        return (((y >= startY && y <= endY) || (y <= this.yPos && y >= this.yPos - dy - circle_radius)) && (x >= startX && x <= endX));
     };
     this.drawPaddle = function () {
+        if(this.reqyPos < 0){
+            this.reqyPos = 0;
+        }
+        if(this.reqyPos > (canvas_height - this.height)){
+            this.reqyPos = canvas_height - this.height;
+        }
         ctx.fillStyle = "#FFFFFF";
-        if (this.dy * (this.yPos - this.reqyPos) < 0) 
+        if ((this.dy * (this.yPos - this.reqyPos) < 0)) {
             this.yPos += this.dy;
+        }
         ctx.fillRect(this.xPos, this.yPos, this.width, this.height);
     };
     this.writeScore = function (score_x, score_y) {
@@ -37,17 +113,22 @@ function Paddle(xPos, yPos) {
         ctx.font = "75px Terminator Two";
         ctx.fillText(this.score, score_x, score_y, 50);
     };
-}
+};
+var paddle1 = new Paddle(50, 200);
+var paddle2 = new Paddle(700, 200);
 
+var circle = function (x, y, r) {
+    'use strict';
 
-
-function circle(x, y, r) {
     ctx.beginPath();
     ctx.arc(x, y, r, 0, Math.PI * 2, true);
     ctx.fill();
-}
+};
 
-function rect(x, y, w, h) {
+var rect = function (x, y, w, h) {
+    'use strict';
+
+    var i, small_x, small_width;
     small_x = 370;
     small_width = 10;
     ctx.beginPath();
@@ -55,29 +136,74 @@ function rect(x, y, w, h) {
     ctx.closePath();
     ctx.fill();
     ctx.fillStyle = "#FFFFFF";
-    cpu.drawPaddle();
-    cpu.writeScore(305, 75);
-    player.drawPaddle();
-    player.writeScore(400, 75);
-    for (var i = 5; i < 500; i += 30) {
+    paddle1.drawPaddle();
+    paddle1.writeScore(305, 75);
+    paddle2.drawPaddle();
+    paddle2.writeScore(400, 75);
+    for (i = 5; i < 500; i += 30) {
         ctx.fillRect(small_x, i, small_width, small_width);
+    }
+};
+
+var resetBall = function () {
+    'use strict';
+
+    var tempX, tempY;
+    x = canvas_width / 2;
+    y = canvas_height / 2;
+    tempX = dx;
+    tempY = dy;
+    tryAndMove(x,y,dx,dy);
+    tryAndMove2(x,y,dx,dy);
+    dx = dy = 0;
+    setTimeout(function () {
+        dx = tempX;
+        dy = tempY;
+    }, 1000);
+};
+
+var clear = function () {
+    'use strict';
+
+    ctx.clearRect(0, 0, canvas_width, canvas_height);
+};
+
+var tryAndMove = function(x,y,dx,dy){
+    if((x > paddle1.xPos) && (dx < 0)){
+        var yPos = y + Math.abs(((x - (paddle1.xPos + 20))/dx)) * dy;
+        while(yPos < 0 || yPos > canvas_height){
+            if(yPos > canvas_height){
+              yPos = canvas_height - (yPos - canvas_height);
+            }else if(yPos < 0){
+             yPos *= -1;
+            }
+        }
+        var speed = Math.abs(paddle1.dy);
+        paddle1.dy = yPos < paddle1.yPos ? -speed : speed;
+        paddle1.reqyPos = Math.round(yPos -40);
+    }
+}
+
+var tryAndMove2 = function(x,y,dx,dy){
+    if((x < paddle2.xPos) && (dx > 0)){
+        var yPos = y + Math.abs(((x - (paddle2.xPos + 20))/dx)) * dy;
+        while(yPos < 0 || yPos > canvas_height){
+            if(yPos > canvas_height){
+              yPos = canvas_height - (yPos - canvas_height);
+            }else if(yPos < 0){
+             yPos *= -1;
+            }
+        }
+        var speed = Math.abs(paddle2.dy);
+        paddle2.dy = yPos < paddle2.yPos ? -speed : speed;
+        paddle2.reqyPos = Math.round(yPos -40);
     }
 }
 
 
-function clear() {
-    ctx.clearRect(0, 0, canvas_width, canvas_height);
-}
+var draw = function () {
+    'use strict';
 
-function init() {
-    canvas = document.getElementById("canvas");
-    ctx = canvas.getContext("2d");
-    x = x_startPos;
-    y = y_startPos;
-    return setInterval(draw, 10);
-}
-
-function draw() {
     clear();
     ctx.fillStyle = "#000000";
     rect(0, 0, canvas_width, canvas_height);
@@ -87,46 +213,44 @@ function draw() {
     var pos = markPositions(3);
     drawPositions(canvas, ctx, pos);
 
-    if (cpu.hitsHorizontalFace(x + dx, y + dy) || player.hitsHorizontalFace(x + dx, y + dy)) dx = -dx;
-    else if (y + dy + circle_radius > canvas_height || y + dy - circle_radius < 0 || cpu.hitsVerticalFace(x + dx, y + dy) || player.hitsVerticalFace(x + dx, y + dy)) dy = -dy;
-    else if (x + dx + circle_radius > canvas_width) {
-        cpu.score++;
+    if (paddle1.hitsHorizontalFace(x + dx, y + dy) || paddle2.hitsHorizontalFace(x + dx, y + dy)) {
+        dx = -dx;
+        tryAndMove(x,y,dx,dy);
+        tryAndMove2(x,y,dx,dy);
+    } else if (y + dy + circle_radius > canvas_height || y + dy - circle_radius < 0 || paddle1.hitsVerticalFace(x + dx, y + dy) || paddle2.hitsVerticalFace(x + dx, y + dy)) {
+        dy = -dy;
+        tryAndMove(x,y,dx,dy);
+        tryAndMove2(x,y,dx,dy);
+    } else if (x + dx + circle_radius > canvas_width) {
+        paddle1.score += 1;
         dx = -dx;
         resetBall();
     } else if (x + dx - circle_radius < 0) {
-        player.score++;
+        paddle2.score += 1;
         dx = -dx;
         resetBall();
     }
-
     x += dx;
     y += dy;
-}
 
-var resetBall = function () {
-    x = canvas_width / 2;
-    y = canvas_height / 2;
-}
-
-var markPositions = function (n) {
-    var pos = [];
-    for (var i = 0; i < n; i++) {
-        pos.push((canvas_height * (i + 1) / n) - (canvas_height) / (2 * n));
-    }
-    return pos;
-}
-
-var drawPositions = function (canvas, ctx, pos) {
-    for (var i = 0; i < pos.length; ++i) {
-        ctx.font = "18px Terminator Two";
-        ctx.fillStyle = "white";
-        ctx.fillText((i + 1).toString(), 650, pos[i]);
-    }
 };
 
-init();
+var init = function () {
+    'use strict';
 
-//Type racer
+    var i;
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+    x = x_startPos;
+    y = y_startPos;
+
+    for (i = 0; i < 3; i += 1) {
+        currentWords.push('placeholder');
+        updateWords(i);
+    }
+    return setInterval(draw, 10);
+};
+
 window.onkeyup = (function () {
     'use strict';
     var i, typeElem, pos, newyPos;
@@ -135,24 +259,55 @@ window.onkeyup = (function () {
     pos = markPositions(3);
 
     return function () {
-        var typed;
+        var typed, typedLC, currentWordLC;
         typed = typeElem.val();
-        var direction = Math.abs(player.dy);
-        if (typed.toLowerCase() === 'first') {
-            newyPos = pos[0] - player.height / 2;
-            player.dy = newyPos < player.yPos ? -direction : direction;
-            player.reqyPos = Math.round(newyPos);
-            typeElem.val('');
-        } else if (typed.toLowerCase() === 'second') {
-            newyPos = pos[1] - player.height / 2;
-            player.dy = newyPos < player.yPos ? -direction : direction;
-            player.reqyPos = Math.round(newyPos);
-            typeElem.val('');
-        } else if (typed.toLowerCase() === 'third') {
-            newyPos = pos[2] - player.height / 2;
-            player.dy = newyPos < player.yPos ? -direction : direction;
-            player.reqyPos = Math.round(newyPos);
-            typeElem.val('');
+        typedLC = typed.toLowerCase();
+
+        for (i = 0; i < currentWords.length; i += 1) {
+            currentWordLC = currentWords[i].toLowerCase();
+            if (typedLC === currentWordLC.substring(0, typed.length) && typed) {
+                updateColor(i, 'green');
+            } else if (typed.length > 1 && typedLC.substring(0, typed.length - 1) === currentWordLC.substring(0, typed.length - 1)) {
+                updateColor(i, 'red');
+            } else if (!typed) {
+                resetAllColors();
+            }
+
+            if (typedLC === currentWordLC) {
+                newyPos = pos[i] - paddle2.height / 2;
+                paddle2.dy = newyPos < paddle2.yPos ? -2 : 2;
+                paddle2.reqyPos = Math.round(newyPos);
+                typeElem.val('');
+                updateWords(i);
+                resetAllColors();
+            }
         }
     };
 }());
+
+var fetchWords = (function () {
+    'use strict';
+    var currentLevel, scriptRoot;
+    currentLevel = 0;
+    scriptRoot = 'http://mystical-hawk-370.appspot.com/';
+
+    return function () {
+        $.ajax({
+            url: scriptRoot + '_loadwords',
+            async: false,
+            data: {
+                level: currentLevel
+            },
+            dataType: 'json',
+            success: function (data) {
+                currentLevel += 1;
+                $.each(data, function (key, val) {
+                    wordList.push(val);
+                });
+            }
+        });
+    };
+}());
+
+fetchWords();
+init();
