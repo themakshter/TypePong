15a6606f6c065e074f426a0ee5be56b716e22b03
Tristan <tristan@bunyan.com>
2013-12-12 16:36:11 +0000

    FIXED FACEBOOK LOGIN!!! YAYYY. Hackiest code yet!

--- scores.py
+++ scores.py
@@ -20,21 +20,64 @@ class UpdateScore(RequestHandler):
             for u in users:
                 u.hiScore = hiScore
                 u.put()
-				
+
+def newRankings(myScore, theirScore, points):#points will be 1 or 0
+    #eA = 1/1+10^(theirs-mine)/400
+
+    k=32
+
+    eAdenomiator = 1+10**((theirScore-myScore)/400)
+    eA = 1/eAdenomiator;
+
+
+    eBdenomiator = 1+10**((myScore-theirScore)/400)
+    eB = 1/eBdenomiator;
+
+    newMyScore = myScore + k*(points-eA)
+
+    newTheirScore = theirScore+ k*(1-points-eB);
+
+    return newMyScore, newTheirScore
+
 class UpdatePVPRating(RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
 
         username = cgi.escape(self.request.get("username"))
         score = cgi.escape(self.request.get("pvpRating"))
-		
+        opponent= cgi.escape(self.request.get("oppositionUsername"))
+        winner = cgi.escape(self.request.get("winner"))
+
+
+        winner=str(winner)
         username = str(username)
         pvpRating = int(score)
+        opponent = str(opponent)
+
+        if winner == username:
+            points =1
+        elif winner == opponent:
+            points =0
+        else:
+            print "SHITFUCKSHIT"#TODO die here.
+
+        if (not opponent == ""):
+            users = db.GqlQuery("SELECT * FROM Player WHERE username = :1", opponent)
+            for u in users:
+                opponentPVPRating = u.pvpRating;
+
+        newRankings(pvpRating, opponentPVPRating, points)
 
         if (not username == ""):
             users = db.GqlQuery("SELECT * FROM Player WHERE username = :1", username)
             for u in users:
                 u.pvpRating = pvpRating
+                u.put()
+
+        if (not username == ""):
+            users = db.GqlQuery("SELECT * FROM Player WHERE username = :1", opponent)
+            for u in users:
+                u.pvpRating = opponentPVPRating
                 u.put()
 
 class UpdateCampaignLevel(RequestHandler):
@@ -43,7 +86,7 @@ class UpdateCampaignLevel(RequestHandler):
 
         username = cgi.escape(self.request.get("username"))
         campaignLevel = cgi.escape(self.request.get("campaignLevel"))
-		
+
         username = str(username)
         campaignLevel = int(campaignLevel)
 
