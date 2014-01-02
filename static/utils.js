92c662475dfc3cf2b5108fabe07c0e7ccc756e8e
Sebastian Claici <sebastianclaici@gmail.com>
2014-01-02 10:30:20 +0200

    Added fade-out message.

--- static/utils.js
+++ static/utils.js
@@ -44,6 +44,38 @@ var hideMessage = function () {
     ctx2.clearRect(0, 0, layer2.width, layer2.height);
 };
 
+var fadeMessage = function (message) {
+    'use strict';
+
+    var layer2, ctx2, alpha, fadeID;
+
+    pauseGame();
+
+    $(canvas).addClass("pongblur");
+    layer2 = document.getElementById("layer2");
+    ctx2 = layer2.getContext("2d");
+
+    ctx2.font = "50px Share Tech";
+    ctx2.fillStyle = "#FFFFFF";
+
+    alpha = 1;
+    fadeID = setInterval(function () {
+        alpha -= .05;
+        if (alpha < 0) {
+            clearInterval(fadeID);
+            hideMessage();
+            resumeGame();
+            ctx2.globalAlpha = 1;
+            return;
+        }
+        ctx2.globalAlpha = alpha;
+
+        ctx2.clearRect(0, 0, layer2.width, layer2.height);
+        wrapText(ctx2, message, canvas.width / 2, canvas.height / 2 - 50,
+            3 * canvas.width / 4, 50);
+    }, 20);
+};
+
 var wrapText = function (context, text, x, y, maxWidth, lineHeight) {
     var words = text.split(' ');
     var line = '';
