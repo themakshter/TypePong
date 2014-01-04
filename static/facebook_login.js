b95121c0d2325c5fb06503cdf4887c452ff325bb
Henco Appel <hencoappel@gmail.com>
2014-01-04 20:44:38 +0000

    should go directly to new page rather than reload

--- static/facebook_login.js
+++ static/facebook_login.js
@@ -7,10 +7,7 @@ window.fbAsyncInit = function() {
         oauth      : true
     });
     FB.Event.subscribe('auth.login', function(response) {
-        window.location.reload();
-    });
-    FB.Event.subscribe('auth.logout', function(response) {
-        window.location.reload();
+	    handleResponse(response);
     });
 
 function handleResponse(response){
