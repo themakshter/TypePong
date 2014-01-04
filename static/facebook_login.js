9bfd81a9130b7a8b16a5b3716b863f4692077165
Henco Appel <hencoappel@gmail.com>
2014-01-04 20:16:20 +0000

    forgot to add the facebook login script file

---
+++ static/facebook_login.js
@@ -0,0 +1,37 @@
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '203715726482941',
+        status     : true,
+        cookie     : true,
+        xfbml      : true,
+        oauth      : true
+    });
+    FB.Event.subscribe('auth.login', function(response) {
+        window.location.reload();
+    });
+    FB.Event.subscribe('auth.logout', function(response) {
+        window.location.reload();
+    });
+
+function handleResponse(response){
+    'use strict'
+    if (response.status ==='connected'){
+        var id =response.authResponse.userID;
+        window.location.href = "_facebookLogin?facebookID=" + id;
+    }
+}
+
+FB.getLoginStatus(handleResponse);
+};
+
+(function(d){
+    var js, id = 'facebook-jssdk';
+    if (d.getElementById(id)) {
+        return;
+    }
+    js = d.createElement('script');
+    js.id = id;
+    js.async = true;
+    js.src = "https://connect.facebook.net/en_US/all.js";
+    d.getElementsByTagName('head')[0].appendChild(js);
+}(document));
