9e0b0ef0d4853a124b5be347a79b5b649e7408cf
Henco Appel <hencoappel@gmail.com>
2014-01-07 12:08:19 +0000

    change tabs to spaces

--- statistics.py
+++ statistics.py
@@ -2,15 +2,15 @@ from google.appengine.ext import db
 from google.appengine.ext.db import polymodel
 
 class Statistics(polymodel.PolyModel):
-	''' All statistics stored have a username and a date'''
+    ''' All statistics stored have a username and a date'''
     username = db.StringProperty(required=True)
     date = db.DateTimeProperty(required=True)
 
 class ChallengeStats(Statistics):
-	'''Challenge mode statistics store how long the player survived'''
+    '''Challenge mode statistics store how long the player survived'''
     survived = db.StringProperty(required=True)
 
 class PvPStats(Statistics):
-	'''PvP stats store the opponent's name and rating'''
+    '''PvP stats store the opponent's name and rating'''
     opponent = db.StringProperty(required=True)
     rating_change = db.IntegerProperty(required=True)
